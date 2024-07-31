025cd8f0a2baa3a188df93eb61f7a35c1c614168
stargood111 <stargood37@naver.com>
2024-07-31 23:05:17 +0900

    @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,6 +39,58 @@ public class BasicItemController {
     public String item(@PathVariable("itemId") long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+        return "basic/item";
+    }
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+//    @PostMapping("/add")
+//    public String save (@RequestParam("itemName") String itemName, /*스프링 3.2부턴 어노테이션에 변수명 정획히 붙여줘야함*/
+//                        @RequestParam("price") int price,
+//                        @RequestParam("quantity") int quantity,
+//                        Model model){
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+//
+//        itemRepository.save(item);
+//
+//        model.addAttribute("item",item);
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+//    public String saveItemV1(@ModelAttribute("item") Item item, Model model){
+//
+//
+//        itemRepository.save(item);
+//        model.addAttribute("item",item);
+//
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+//    public String saveItemV2(@ModelAttribute("item") Item item){
+//
+//        itemRepository.save(item);
+////        model.addAttribute("item",item); /*@ModelAttribute사용시 생략가능 */
+//
+//        return "basic/item";
+//    }
+//    @PostMapping("/add")
+//    public String saveItemV3(@ModelAttribute Item item){
+//
+//        itemRepository.save(item);
+//
+//        return "basic/item";
+
+    @PostMapping("/add")
+    public String saveItemV4(Item item){
+
+        itemRepository.save(item);
+
         return "basic/item";
     }
 
